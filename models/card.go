288409284bbc57027fd2c2dc6a59f0671df14110
mohod24 <mohdmusyaffa.123140184@student.itera.ac.id>
2025-12-20 21:37:15 +0700

    completing all files in models

---
+++ models/card.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Card struct {
+	InternalID  int64      `json:"internal_id" db:"internal_id" gorm:"primaryKey;autoIncrement"`
+	PublicID    uuid.UUID  `json:"public_id" db:"public_id"`
+	ListID      int64      `json:"list_internal_id" db:"list_internal_id" gorm:"column:list_internal_id"`
+	Title       string     `json:"title" db:"title"`
+	Description string     `json:"description" db:"description"`
+	DueDate     *time.Time `json:"due_date,omitempty" db:"due_date"`
+	Position    int        `json:"position" db:"position"`
+	CreatedAt   time.Time  `json:"created_at" db:"created_at"`
+
+	// relasi
+	Assigness   []CardAssignee   `json:"assigness,omitempty" gorm:"foreignKey:CardID;reference:InternalID"`
+	Attachments []CardAttachment `json:"attachments,omitempty" gorm:"foreignKey:CardID;references:InternalID"`
+	Labels      []CardLabel      `json:"labels,omitempty" gorm:"foreignKey:CardID;references:InternalID"`
+}
