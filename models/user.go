3b79d3f4c9b716724dd7a091e2f1467292ed344d
mohod24 <mohdmusyaffa.123140184@student.itera.ac.id>
2025-12-20 20:29:47 +0700

    add UserResponse struct

--- models/user.go
+++ models/user.go
@@ -8,13 +8,23 @@ import (
 )
 
 type User struct {
-	InternalID int64 `json:"internal_id" db:"internal_id" gorm:"primarykey"`
-	PublicID   uuid.UUID `json:"public_id" db:"public_id"`
-	Name	   string    `json:"name" db:"name"`
-	Email	  string    `json:"email" db:"email" gorm:"unique"`
-	Password   string    `json:"-" db:"password" gorm:"column:pwd"`
-	Role	   string    `json:"role" db:"role"`
-	CreatedAt  time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at" db:"updated_at"`
-	DeletedAt  gorm.DeletedAt `json:- gorm:"index"`
+	InternalID int64          `json:"internal_id" db:"internal_id" gorm:"primaryKey"`
+	PublicID   uuid.UUID      `json:"public_id" db:"public_id" gorm:"column:public_id"`
+	Name       string         `json:"name" db:"name"`
+	Email      string         `json:"email" db:"email" gorm:"unique"`
+	Password   string         `json:"password" db:"password" gorm:"column:password"`
+	Role       string         `json:"role" db:"role"`
+	CreatedAt  time.Time      `json:"created_at" db:"created_at"`
+	UpdatedAt  time.Time      `json:"updated_at" db:"updated_at"`
+	DeletedAt  gorm.DeletedAt `json:"-" gorm:"index"`
+}
+
+type UserResponse struct {
+	PublicID  uuid.UUID      `json:"public_id" `
+	Name      string         `json:"name" `
+	Email     string         `json:"email" `
+	Role      string         `json:"role" `
+	CreatedAt time.Time      `json:"created_at" `
+	UpdatedAt time.Time      `json:"updated_at" `
+	DeletedAt gorm.DeletedAt `json:"-"`
 }
