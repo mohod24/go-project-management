4f87022174203903cd4e0daa7aae827f9695d154
mohod24 <mohdmusyaffa.123140184@student.itera.ac.id>
2025-12-20 22:54:11 +0700

    add utils/jwt.go for jwt configuration

---
+++ utils/jwt.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/mohod24/go-project-management/config"
+)
+
+//generatetoken jwt
+//generate refresh token
+
+func GenerateToken(userID int64, role, email string, publicID uuid.UUID) (string, error) {
+	secret := config.AppConfig.JWTSecret
+	duration, _ := time.ParseDuration(config.AppConfig.JWTExpire)
+
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"role":    role,
+		"pub_id":  publicID,
+		"email":   email,
+		"exp":     time.Now().Add(duration).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
+
+func GenerateRefreshToken(userID int64) (string, error) {
+	secret := config.AppConfig.JWTSecret
+	duration, _ := time.ParseDuration(config.AppConfig.JWTRefreshToken)
+
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(duration).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
